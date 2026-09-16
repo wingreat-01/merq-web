5ba8b3984d1194876dc624c1d9a27bffa1152dc2
chad <chad.yunitrade@gmail.com>
2026-09-16 10:03:52 +0800

    added theme on mobile and web

--- dist/app/flutter_bootstrap.js
+++ dist/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "515728518" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3573353404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
