c6f277c47f7c21c9ccedb965952bb79f929bbd3d
chad <chad.yunitrade@gmail.com>
2026-09-19 15:51:49 +0800

    added currency picker

--- dist/app/flutter_bootstrap.js
+++ dist/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2173418409" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3665120897" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
